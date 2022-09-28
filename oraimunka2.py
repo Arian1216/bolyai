4890cb0aa191c631314366245dd16efc245f3efb
roxinan <roxinarian2@gmail.com>
2022-09-28 12:29:43 +0200

    2022.09.28. (oraimunka2)

---
+++ oraimunka2.py
@@ -0,0 +1,6 @@
+nev=input("Kérem adja meg a vezetéknevét:")
+nev1=input("Kérem adja meg a keresznevét:")
+szuletesiev=int(input("Kérem adja meg a szültési évét:"))
+print(29*"*")
+print("Az ön  neve:", nev, nev1,)
+print("Az életkora:", 2022-szuletesiev)
