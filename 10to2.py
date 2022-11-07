c1ce3e9b009db9977a062226b3b046eb6781f60d
RoxinA05 <roxin.arian.nathanael.tech2021c@bolyaimovar.com>
2022-11-07 19:07:53 +0100

    2022.11.07.

--- 10to2.py
+++ 10to2.py
@@ -1,7 +1,8 @@
+
 tizes = int(input("Kérem adjon meg egy számot (0-255): "))
 kettes = ""
 
-while tizes > 0:
+while tizes > 0 or tizes < 256:
     kettes = (str(tizes % 2) + kettes)
     tizes = tizes // 2
 
