019350679852c400b7dfb039998f0ac36567605b
roxinan <roxinarian2@gmail.com>
2022-11-09 12:29:54 +0100

    2022.11.09.

---
+++ om10.py
@@ -0,0 +1,8 @@
+szavak = ["sonka", "sajt", "lekvár", "csokoládé"]
+szavak[2:2] = ["méz",16,"CL"]
+print(szavak) #sonka = 0, sajt = 1, MÉZ = 2 -> 2:2
+list = ["sonka", "sajt", "lekvár", "csokoládé"]
+list[1:3] = ["sali","csirke","a"]
+print(list)
+
+#lecke: kimenet = honap + szam
