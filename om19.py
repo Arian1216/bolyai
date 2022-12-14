d4745dd75a2cca16b96e27534ed24b0a89676e24
roxinan <roxinarian2@gmail.com>
2022-12-14 12:30:37 +0100

    2022.12.14.

---
+++ om19.py
@@ -0,0 +1,19 @@
+def osszead(x,y):
+    return x + y
+def kivon(x,y):
+    return x - y
+def szorzas(x,y):
+    return x * y
+def osztas(x,y):
+    if x or y == 0:
+        print("Nem jo a 0.")
+    return x / y
+def negyzet(x,y):
+    return x ** y
+def fakt(x,):
+   return x
+
+muvelet = input("adja meg a muveletet: ")
+sym = ["+","-","*",":","^","!"]
+for i in range(len(muvelet)):
+    
