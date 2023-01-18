b7913d3a8886349a4da5c82da49773a0190bd60e
roxinan <roxinarian2@gmail.com>
2023-01-18 12:29:16 +0100

    2023.01.18.

--- om22.py
+++ om22.py
@@ -14,5 +14,43 @@ while dictionary != words:
     else:
         print(words,"=","is not in dictionary")
 
-#-----------------------------------------------------------------------------------------------------------------------
+################################################################
+
+dictionary2 = {
+              "cat": "cica",
+              "dog": "kutya",
+              "horse": "ló"
+              }
+#dictionary2['cat'] = 'macska'
+
+word = input("Cserelje ki a kulcsszot: ")
+dictionary2['cat'] = word
+print(dictionary2)
+
+################################################################
+
+dictionary3 = {
+              "cat": "cica",
+              "dog": "kutya",
+              "horse": "ló"
+              }
+for i in range(10):
+    new_word = input("Uj szo: ")
+    new_key = input("Uj kulcsszo: ")
+    dictionary3.update({new_word:new_key})
+print(dictionary3)
+
+################################################################
+
+szotar = {
+        "table":"asztal",
+        "key":"kulcs",
+        "value":"ertek"
+}
+for english, magyar in szotar.items():
+    print(english,"=",magyar)
+for magyar in szotar.values():
+    print(magyar)
+
+################################################################
 
