383494d72ef6bc72c03598408088f97bea035a66
roxinan <roxinarian2@gmail.com>
2023-03-08 12:29:02 +0100

    2023.03.08. oraimunka:))))))

--- om30.py
+++ om30.py
@@ -1,14 +1,16 @@
 countries = []
+def founder(_founders):
+    return _founders['Accession']
+
 with open('eu.csv','r',encoding='utf-8') as source:
-    #for line in source:
-        #print(line.strip().split(',')) listaba helyezes
+
     for line in source:
         country_list=line.strip().split(',')
         country = {'Country': country_list[0], 'Capital': country_list[1], 'Accession': country_list[2]}
         countries.append(country)
-#print(countries)
-#for country in countries:
-    #print(country['Country']) #'fovarosa',country['Capital'],country['Accession'],'-ben csatlakozott.')
-for country in countries:
-    year = int(country['Accession'])
-    print(year)
+
+countries.sort(key=founder)
+
+print(countries)
+
+
