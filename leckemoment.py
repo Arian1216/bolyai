f8a143f9a60c0f8687e10d5517344b8430c724d0
saekq <73390677+saekq@users.noreply.github.com>
2023-03-14 18:49:01 +0000

    Optimalized Python Word Counter

--- leckemoment.py
+++ leckemoment.py
@@ -1,15 +1,23 @@
+#Optimalizalt kod + feature)
+
+import re
+
 sum = 0
-
+legrovidebb_szo = ''
+leghosszabb_szo = ''
 with open('parduc_2.feladat.txt', 'r', encoding='utf-8') as source:
-    for lines in source:
-        vers = source.read()
+vers = ''.join(line for line in source)
 
-    letters = len(vers)
-    words = len(vers.split())
+words = re.findall(r'\b\w+\b', vers)
+for word in words:
+if not legrovidebb_szo or len(word) < len(legrovidebb_szo):
+legrovidebb_szo = word
+if len(word) > len(leghosszabb_szo):
+leghosszabb_szo = word
 
-    for mgh in vers:
-        if mgh in 'aeiouáéíóöőúüűAEIOUÁÉÍÓÖŐÚÜŰ':
-            sum += 1
+sum = sum(1 for c in vers if c in 'aeiouáéíóöőúüűAEIOUÁÉÍÓÖŐÚÜŰ')
 
 print(vers,'\n')
-print('A vers',letters,'betűt és',words,'szót és',sum,'magánhangzót tartalmaz.')
+print('A vers',len(vers),'betűt és',len(words),'szót és',sum,'magánhangzót tartalmaz.')
+print('A legrövidebb szó:', legrovidebb_szo)
+print('A leghosszabb szó:', leghosszabb_szo)
