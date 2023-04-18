aa96b5c5f9001fc84a022f0114129f8cd7b87f97
arian <113454331+Arian1216@users.noreply.github.com>
2023-04-18 09:36:33 +0200

    Update vizsga.py
    oraimunka 04.18

--- vizsga.py
+++ vizsga.py
@@ -1,14 +1,19 @@
-n = int(input('Adj meg egy szamot:'))
-if n % 2 == 0:
-    print('Paros')
-else:
-    print('Paratlan')
+# n = int(input('Adj meg egy szamot:'))
+# if n % 2 == 0:
+#     print('Paros')
+# else:
+#     print('Paratlan')
 
-def email():
-    Name = input('Add meg a neved:')
-    startYear = input('Add meg a kezdo eved:')
-    ClassName = input('Add meg az osztalyod:')
-    return f"{Name.replace(' ','.')}.tech{startYear}{ClassName}@bolyaimovar.com"
-print(email())
+# def email(Name,Year):
+#     #Year = 2023-str(Year-int(_Year[0]-8)+_Year[1]) a yeart darabolni kell listaba stb hogy jo legyen
+#     return f"{Name.replace(' ','.').lower()}.tech{Year}@bolyaimovar.com"
+# Name = input('Add meg a neved:')
+# Year = input('Kerem adja meg az osztalyt')
+# print(email(Name.replace('áéíöüóőúű','aeiou'),Year)) a replace nem mukodik
 
-#with open('hegyekMo.txt','r',encoding='utf-8') as s:
+
+with open('hegyekMo.txt','r',encoding='utf-8') as s:
+    s.readline()
+    mt = [lines.strip('\ufeff').split(';')[2] for lines in s]
+    for height in sorted(mt):
+        print(height)
