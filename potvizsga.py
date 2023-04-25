47f836b78f2f841959e9d45e36425797bee912ff
arian <113454331+Arian1216@users.noreply.github.com>
2023-04-25 09:43:44 +0200

    Add files via upload
    2023.04.25.

--- potvizsga.py
+++ potvizsga.py
@@ -1,27 +1,43 @@
 #1. feladat
 x = int(input('Adjon meg egy szamot: '))
 y = int(input('Adjon meg egy masik szamot: '))
+
 if x > y:
-    print(x)
+     print(x)
 else:
     print(y) 
 
 #2. feladat
-def kodolas():
+def kodolas(_text,_letterToChange):
+
     newText = ''
-    text = input('Adja meg a szoveget: ')
-    letterToChange = input('Adja meg a kicserelendo betut: ')
-    for char in text:
-        if char == letterToChange:
+    for char in _text:
+        if char == _letterToChange:
             newText += str(ord(char))
         else:
             newText += char
-    return newText   
+    return newText
 
-print(kodolas())
+text = input('Adja meg a szoveget: ')
+letterToChange = input('Adja meg a kicserelendo betut: ')
 
-#2. feladat
+print(kodolas(text,letterToChange))
 
-with open('autok.csv','r',encoding='utf-8') as s:
+#3. feladat
+
+acc = 0
+with open('autok.csv','r',encoding='utf-8') as s: #a, feladat
+    s.readline()
+
     cars = [lines.strip().split(';') for lines in s]
-    print(cars)
+
+    #b, feladat
+    for car in cars:
+        if car[0] == 'Munkács' and car[1] == 'Záhony':
+            acc += int(car[4])
+    print(acc)
+    #acc = [_lines.strip().split(',')[4] for _lines in cars]
+    #average = sum(acc)/len(acc)
+
+    #print(average)
+
